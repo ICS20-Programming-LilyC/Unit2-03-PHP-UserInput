94b7d4c295e527818b834560f0b9366dd32faebb
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-03-10 02:29:48 +0000

    added favicon

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 <html lang="en-ca">
   <head>
 
-    <!-- Metadata -->
+    <!-- Meta data -->
     <meta charset="utf-8">
     <meta name="description" content="My first webpage with user input">
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="Lily C">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/favicon-16x16.pngg">
+    <link rel="manifest" href="./fav_index/site.webmanifest">
     
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
